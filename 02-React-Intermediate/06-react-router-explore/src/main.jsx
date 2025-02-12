b13587c893d07eb9dd177cd887c0b339dd30953e
rayanahmed <thismac@rayanahmed.local>
2025-02-12 18:31:08 +0600

    parent folder changed

--- 02-React-Intermediate/06-react-router-explore/src/main.jsx
+++ 02-React-Intermediate/06-react-router-explore/src/main.jsx
@@ -1,7 +1,7 @@
 import "./assets/css/index.css";
 import App from "./App.jsx";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { BrowserRouter } from "react-router";
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
