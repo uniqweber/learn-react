b343baf16fc5438603f4bfcd1c743823e34403d9
rayanahmed <thismac@rayanahmed.Dlink>
2025-02-28 21:44:29 +0600

    basic setup is done

---
+++ Intermediate/05-explore-shadcn-ui/src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+   return (
+      <>
+         <div className="flex items-center justify-center min-h-screen text-5xl font-medium tracking-wide">App</div>
+      </>
+   );
+};
+
+export default App;
