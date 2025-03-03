53185ab857809b87ada0fd18f1533472247f9ff7
rayanahmed <thismac@rayanahmed.Dlink>
2025-03-04 00:38:16 +0600

    shadcn explored

--- Intermediate/05-explore-shadcn-ui/src/App.jsx
+++ Intermediate/05-explore-shadcn-ui/src/App.jsx
@@ -1,7 +1,9 @@
 const App = () => {
    return (
       <>
-         <div className="flex items-center justify-center min-h-screen text-5xl font-medium tracking-wide">App</div>
+         <div className="flex items-center justify-center min-h-screen text-5xl font-medium tracking-wide">
+            <Button>hello</Button>
+         </div>
       </>
    );
 };
--- Intermediate/05-explore-shadcn-ui/vite.config.js
+++ Intermediate/05-explore-shadcn-ui/vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from "vite";
+import path from "path";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import tailwindcss from "tailwindcss";
 
+// https://vitejs.dev/config/
 export default defineConfig({
    plugins: [react(), tailwindcss()],
+   resolve: {
+      alias: {
+         "@": path.resolve("./src"),
+      },
+   },
 });
