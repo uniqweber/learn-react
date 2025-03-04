82bd46435c739c06bca8b9335c0268b2669fdcdc
rayanahmed <thismac@rayanahmed.Dlink>
2025-03-04 16:10:05 +0600

    gsap fromTo explored

--- Intermediate/07-GSAP-animation-explore/src/pages/GsapFrom.jsx
+++ Intermediate/07-GSAP-animation-explore/src/pages/GsapFrom.jsx
@@ -1,4 +1,4 @@
-import { useGSAP } from "@gsap/react";
+import {useGSAP} from "@gsap/react";
 import gsap from "gsap";
 
 const GsapFrom = () => {
@@ -11,31 +11,25 @@ const GsapFrom = () => {
       duration: 2,
       ease: "power1.inOut",
     });
+    // form er kaj holo. she age thekei sore thakbe tar position theke. and pore tar position e ferot ashbe eitai hocche from. mane tar position jodi 0 hoy. ebong amra jodi boli gsap.from(id,{x:200}) tar mane she 200 porjonto shore thakbe. reload diyar sathe dekhabe j she 200 theke 0 er dike ashtese
   }, []);
   return (
     <main>
       <h1>GsapFrom</h1>
 
       <p className="mt-5 text-gray-500">
-        The <code>gsap.from()</code> method is used to animate elements from a
-        new state to their current state.
+        The <code>gsap.from()</code> method is used to animate elements from a new state to their current state.
       </p>
 
       <p className="mt-5 text-gray-500">
-        The <code>gsap.from()</code> method is similar to the{" "}
-        <code>gsap.to()</code> method, but the difference is that the{" "}
-        <code>gsap.from()</code> method animates elements from a new state to
-        their current state, while the <code>gsap.to()</code> method animates
-        elements from their current state to a new state.
+        The <code>gsap.from()</code> method is similar to the <code>gsap.to()</code> method, but the difference is that the <code>gsap.from()</code>{" "}
+        method animates elements from a new state to their current state, while the <code>gsap.to()</code> method animates elements from their current
+        state to a new state.
       </p>
 
       <p className="mt-5 text-gray-500">
         Read more about the{" "}
-        <a
-          href="https://greensock.com/docs/v3/GSAP/gsap.from()"
-          target="_blank"
-          rel="noreferrer noopener nofollow"
-        >
+        <a href="https://greensock.com/docs/v3/GSAP/gsap.from()" target="_blank" rel="noreferrer noopener nofollow">
           gsap.from()
         </a>{" "}
         method.
